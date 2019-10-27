85bf0f3aa4b191fd28f85b31b3e061e31e94aaba
HeKoDev <hekodev@gmail.com>
2019-10-27 13:03:39 -0400

    provider completely set up

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:myshop/models/product.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  // final String title;
-
-  // ProductDetailScreen(this.title);
-
-  static const routeName = '/product-detail-screen';  //Define routeName to be used in the routes map in main
+  static const routeName =
+      '/product-detail-screen'; //Define routeName to be used in the routes map in main
 
   @override
   Widget build(BuildContext context) {
-    final productId = ModalRoute.of(context).settings.arguments as String; //is the id ! 
-    // ...
+    final productId =
+        ModalRoute.of(context).settings.arguments as String; //is the id !
+    final loadProduct = Provider.of<ProductsProvider>(
+      context,
+      listen: false, //with false (and the default is true), this widget wont rebuild
+    ).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadProduct.title),
       ),
     );
   }
